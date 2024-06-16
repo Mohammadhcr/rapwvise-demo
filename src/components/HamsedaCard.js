2b0bcef0042c7b204476ee1a85f107e249b2355b
Mohammad hcr <mohammadhcr97@gmail.com>
2024-06-17 00:04:30 +0330

    Update HamsedaCard.js22

--- src/components/HamsedaCard.js
+++ src/components/HamsedaCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import styles from '../styles/Card.module.scss'
 import { hamsedaDB } from '../database/hamsedaDB';
 
 const HamsedaCard = (props) => {
 
-    const {card, cardPhoto, cardContent, cardTitle, cardP, cards} = styles;
+    const {card, cardPhoto, cardContent, cardTitle, cardSubtitle, cards, cardP} = styles;
 
     return (
         <>
@@ -14,13 +14,12 @@ const HamsedaCard = (props) => {
                 {hamsedaDB.map(podcast =>
                     <Link to={`/hamseda/${podcast.id}`} key={podcast.id}>
                         <div className={card} style={{backgroundImage: `url(${podcast.artwork})`}}>
-                        // <div className={card}>
-                            // <div className={cardPhoto}>
-                                // <img src={podcast.artwork} alt="Artwork" />
-                            // </div>
+                            {/* <div className={cardPhoto}> */}
+                                {/* <img src={comment.artwork} alt="Artwork" /> */}
+                            {/* </div> */}
                             <div className={cardContent}>
                                 <h2 className={cardTitle}>{podcast.episodeNumber}: {podcast.episodeName}</h2>
-                                <p className={cardP}>{podcast.episodeDescription}</p>
+                                <p className={cardP}>مطالعه مطلب <i class='bx bxs-plane-alt'></i></p>
                             </div>
                         </div>
                     </Link>
