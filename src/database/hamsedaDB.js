bf7ca4f399061bc2358bba88bda5e5f0658169ee
Mohammad hcr <mohammadhcr97@gmail.com>
2024-06-24 01:15:21 +0330

    Update hamsedaDB.js7877

--- src/database/hamsedaDB.js
+++ src/database/hamsedaDB.js
@@ -5,10 +5,10 @@ export const hamsedaDB = [
         episodeName: "قاضی منو دوست داشت", 
         episodeDescription: "توی این قسمت با دو نفر درمورد آهنگ «قاضى‌ منو‌ دوست‌ داشت» از هیچ‌کس با آهنگسازی مهدیار گفتگو کردیم تا این قطعه رو از دیدگاه‌های مختلف بررسی کنیم", 
         releaseDate: 2020, 
-        artwork: "https://i.scdn.co/image/ab67616d0000b27323dc26dfeae828fb5383147c", 
-        soundcloud: "https://soundcloud.com/mahdyar/sets/adl", 
-        spotify: "https://open.spotify.com/album/4kn0HuQXr7RVHBMYELIELs?si=QpOSbKCRSSyIDNt979NMjA", 
-        apple: "https://music.apple.com/us/album/adl-ep/580554841"
+        artwork: "https://i1.sndcdn.com/artworks-000054840850-lj79t7-t500x500.jpg", 
+        soundcloud: "https://soundcloud.com/rapwvise", 
+        spotify: "https://open.spotify.com/show/03Ruo9daBN4hEwmUqyRlHk", 
+        apple: "https://podcasts.apple.com/us/podcast/hamseda/id1522223127"
     },
     {
         id: 2, 
@@ -16,10 +16,10 @@ export const hamsedaDB = [
         episodeName: "تولید محتوا در رپ فارسی", 
         episodeDescription: "توی این قسمت با «سام» در مورد تولید محتوا در رپ فارسی گفتگو کردیم", 
         releaseDate: 2020, 
-        artwork: "https://i.scdn.co/image/ab67616d0000b27323dc26dfeae828fb5383147c", 
-        soundcloud: "https://soundcloud.com/mahdyar/sets/adl", 
-        spotify: "https://open.spotify.com/album/4kn0HuQXr7RVHBMYELIELs?si=QpOSbKCRSSyIDNt979NMjA", 
-        apple: "https://music.apple.com/us/album/adl-ep/580554841"
+        artwork: "https://i1.sndcdn.com/artworks-000037630188-zhwaxq-t500x500.jpg", 
+        soundcloud: "https://soundcloud.com/rapwvise", 
+        spotify: "https://open.spotify.com/show/03Ruo9daBN4hEwmUqyRlHk", 
+        apple: "https://podcasts.apple.com/us/podcast/hamseda/id1522223127"
     },
     {
         id: 3, 
@@ -27,10 +27,10 @@ export const hamsedaDB = [
         episodeName: "مسئولیت هنرمند", 
         episodeDescription: "توی این قسمت با «هیراد» درمورد مسئولیت‌هایی که یک هنرمند در قبال مخاطب‌ها و طرفداراش داره گفتگو کردیم", 
         releaseDate: 2020, 
-        artwork: "https://i.scdn.co/image/ab67616d0000b27323dc26dfeae828fb5383147c", 
-        soundcloud: "https://soundcloud.com/mahdyar/sets/adl", 
-        spotify: "https://open.spotify.com/album/4kn0HuQXr7RVHBMYELIELs?si=QpOSbKCRSSyIDNt979NMjA", 
-        apple: "https://music.apple.com/us/album/adl-ep/580554841"
+        artwork: "https://i1.sndcdn.com/artworks-000110013796-5nhzbi-t500x500.jpg", 
+        soundcloud: "https://soundcloud.com/rapwvise", 
+        spotify: "https://open.spotify.com/show/03Ruo9daBN4hEwmUqyRlHk", 
+        apple: "https://podcasts.apple.com/us/podcast/hamseda/id1522223127"
     },
     {
         id: 4, 
@@ -38,10 +38,10 @@ export const hamsedaDB = [
         episodeName: "روابط حرفه‌ای هنرمند", 
         episodeDescription: "توی این قسمت با «علیرضا» درمورد انواع روابط و همکاری‌ها و قرارداد‌ها بین هنرمندان در رپ فارسی صحبت کردیم", 
         releaseDate: 2020, 
-        artwork: "https://i.scdn.co/image/ab67616d0000b27323dc26dfeae828fb5383147c", 
-        soundcloud: "https://soundcloud.com/mahdyar/sets/adl", 
-        spotify: "https://open.spotify.com/album/4kn0HuQXr7RVHBMYELIELs?si=QpOSbKCRSSyIDNt979NMjA", 
-        apple: "https://music.apple.com/us/album/adl-ep/580554841"
+        artwork: "https://i1.sndcdn.com/artworks-000151176981-5a20as-t500x500.jpg", 
+        soundcloud: "https://soundcloud.com/rapwvise", 
+        spotify: "https://open.spotify.com/show/03Ruo9daBN4hEwmUqyRlHk", 
+        apple: "https://podcasts.apple.com/us/podcast/hamseda/id1522223127"
     },
     {
         id: 5, 
@@ -49,9 +49,9 @@ export const hamsedaDB = [
         episodeName: "بهرام", 
         episodeDescription: "اولین روایت همصدا - بهرام", 
         releaseDate: 2021, 
-        artwork: "https://i.scdn.co/image/ab67616d0000b27323dc26dfeae828fb5383147c", 
-        soundcloud: "https://soundcloud.com/mahdyar/sets/adl", 
-        spotify: "https://open.spotify.com/album/4kn0HuQXr7RVHBMYELIELs?si=QpOSbKCRSSyIDNt979NMjA", 
-        apple: "https://music.apple.com/us/album/adl-ep/580554841"
+        artwork: "https://i1.sndcdn.com/artworks-CmgdPdkvmevQPRgf-jXqxZw-t500x500.jpg", 
+        soundcloud: "https://soundcloud.com/rapwvise", 
+        spotify: "https://open.spotify.com/show/03Ruo9daBN4hEwmUqyRlHk", 
+        apple: "https://podcasts.apple.com/us/podcast/hamseda/id1522223127"
     },
 ]
